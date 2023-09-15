ea4f533f70617d7e1909badabd035d3681b5c04f
lian <bagiyanliana2@gmail.com>
2023-09-15 17:09:59 +0400

    destructure from global redux state

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -4,9 +4,8 @@ import watchLaterSlice from '../data/watchLaterSlice'
 import placeholder from '../assets/not-found-500X750.jpeg'
 
 const Movie = ({ movie, viewTrailer, closeCard }) => {
-
-    const state = useSelector((state) => state)
-    const { starred, watchLater } = state
+    const starredMovies = useSelector(state => state.starred.starredMovies);
+    const watchLaterMovies = useSelector(state => state.watchLater.watchLaterMovies);
     const { starMovie, unstarMovie } = starredSlice.actions
     const { addToWatchLater, removeFromWatchLater } = watchLaterSlice.actions
 
@@ -26,7 +25,7 @@ const Movie = ({ movie, viewTrailer, closeCard }) => {
                 <div className="info_panel">
                     <div className="overview">{movie.overview}</div>
                     <div className="year">{movie.release_date?.substring(0, 4)}</div>
-                    {!starred.starredMovies.map(movie => movie.id).includes(movie.id) ? (
+                    {!starredMovies.map(movie => movie.id).includes(movie.id) ? (
                         <span className="btn-star" data-testid="starred-link" onClick={() => 
                             dispatch(starMovie({
                                 id: movie.id, 
@@ -43,7 +42,7 @@ const Movie = ({ movie, viewTrailer, closeCard }) => {
                             <i className="bi bi-star-fill" data-testid="star-fill" />
                         </span>
                     )}
-                    {!watchLater.watchLaterMovies.map(movie => movie.id).includes(movie.id) ? (
+                    {!watchLaterMovies.map(movie => movie.id).includes(movie.id) ? (
                         <button type="button" data-testid="watch-later" className="btn btn-light btn-watch-later" onClick={() => dispatch(addToWatchLater({
                                 id: movie.id, 
                                 overview: movie.overview, 
